24c6404e6055a0703b6feba896a8f144185bcffb
Simon <simonkdeveloper@gmail.com>
2019-09-30 11:21:22 -0400

    123

---
+++ cardio1.js
@@ -0,0 +1,35 @@
+// 1. String Reversal
+const reverseString = (str) => {
+
+    // method 1 - build in array methods
+    //return str.split("").reverse().join("");
+
+    //method 2 - reversed for loop
+    // let revStr = "";
+    // for (let i = str.length; i >= 0; i--) {
+    //     revStr += str.substring(i, i + 1);
+    // }
+    // return revStr;
+
+    //method 3 - reduce
+    return str.split("").reduce((acc, cur) => {
+        return cur + acc;
+    }, "")
+}
+
+console.log(reverseString("hello"));
+
+// 2. Validate Palindromes
+
+
+// 3. Integer Reversal
+
+
+// 4. Capitalize Letters
+
+
+// 5. Find The Max Character
+
+
+// 6. FizzBuzz Challenge
+
